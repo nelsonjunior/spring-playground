4c067c773811240142111e9e88f0d733228fe293
Nelson <nrsjnet@gmail.com>
2021-01-20 23:51:44 -0300

    inclusão curso spring web flux

---
+++ spring-webflux-essentials/src/main/java/academy/devdojo/webflux/controller/AnimeController.java
@@ -0,0 +1,37 @@
+package academy.devdojo.webflux.controller;
+
+import academy.devdojo.webflux.domain.Anime;
+import academy.devdojo.webflux.repository.AnimeRepository;
+import academy.devdojo.webflux.service.AnimeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.text.MessageFormat;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("animes")
+@Slf4j
+public class AnimeController {
+
+    private final AnimeService animeService;
+
+    @GetMapping
+    public Flux<Anime> listAll(){
+        log.info("list all anime!");
+        return animeService.findAll();
+    }
+
+    @GetMapping(path = "{id}")
+    public Mono<Anime> listById(@PathVariable int id){
+        log.info(MessageFormat.format("list anime by id {0}!", id));
+        return animeService.findById(id);
+    }
+}
